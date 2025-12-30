c2a2084e207a359ab5012c446d7728b0bf9708b3
Michael Blithe <michaelrblithe@gmail.com>
2025-12-30 16:18:04 -0500

    Add default

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -7,6 +7,9 @@
     ./disko.nix
     
   ];
+  hardware.enableRedistributableFirmware = true;
+  hardware.firmware = [ pkgs.linux-firmware ];
+  networking.networkmanager.enable = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   home-manager.users.alex.home.stateVersion = "25.11";
