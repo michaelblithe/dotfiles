a022284cc160085170b820925ffe206687c89e35
Alex Blithe <michaelrblithe@gmail.com>
2026-01-02 18:37:01 -0500

    Bunch of updates

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -8,57 +8,73 @@
         layer = "top";
         height = 34;
         position = "top";
-        
+
         modules-left = [
           "hyprland/workspaces"
         ];
-        
+
         modules-center = [
           "hyprland/window"
         ];
-        
+
         modules-right = [
+          "battery"
           "pulseaudio"
           "network"
+          "idle_inhibitor"
           "clock"
         ];
-        
+
         # Module configurations
         "hyprland/workspaces" = {
           on-click = "activate";
           format = "{name}";
         };
-        
+
         "hyprland/window" = {
           separate-outputs = true;
           max-length = 80;
         };
-        
+
         pulseaudio = {
           format = "{volume}% 🔊";
           format-muted = "muted 🔇";
           scroll-step = 5;
           on-click = "pavucontrol";
         };
-        
+
         network = {
           format-wifi = "{essid} {signalStrength}%";
           format-ethernet = " {ifname}";
           format-disconnected = "disconnected";
           tooltip = false;
         };
-        
+
+        battery = {
+          format = "{capacity}% ({time}) 🔋";
+          format-charging = "⚡ {capacity}% ({time})";
+          format-discharging = "🔋 {capacity}% ({time})";
+        };
+        idle_inhibitor = {
+          format = "{status}";
+        };
+
         clock = {
-          format = "{:%I:%M %p}";
-          format-alt = "{:%d-%m-%Y}";
+          interval = 1;
+          timezone = "America/New_York";
+          format = "{:%a %b %d %I:%M%p}";
+          on-click = "hyprctl dispatch exec [float] 'gnome-calendar'";
+          tooltip = true;
+          tooltip-format = "{:%a %b %d %I:%M%p}";
         };
       };
     };
-    
+
     style = ''
       * {
-        font-family: JetBrainsMono, monospace;
-        font-size: 11pt;
+
+        font-family: Pixel Operator Mono, JetBrainsMono, monospace;
+        font-size: 14pt;
         color: #ffffff;
       }
 
@@ -84,6 +100,7 @@
       #clock, #network, #pulseaudio {
         padding: 0 10px;
       }
+
     '';
   };
 }
