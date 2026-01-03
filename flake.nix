c18e728e94cf9d96da15e537a5e62730e6466576
Michael Blithe <michaelrblithe@gmail.com>
2026-01-03 12:05:42 -0500

    Sops

--- flake.nix
+++ flake.nix
@@ -3,79 +3,116 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+    };
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-    nur.url = "github:nix-community/NUR";
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, disko, nix-vscode-extensions, nur, ... }: 
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      disko,
+      nix-vscode-extensions,
+      nur,
+      sops-nix,
+      ...
+    }:
 
-    let 
-      configuration = { pkgs, ... }: { 
-        nixpkgs.config.allowUnfree = true;
-        nixpkgs.overlays = [
-          nix-vscode-extensions.overlays.default
-          nur.overlays.default
-        ];
-      };
-    in  {
-    nixosConfigurations = {
-      thinkpad = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          { 
-            nixpkgs.overlays = [ 
-              nix-vscode-extensions.overlays.default
-              nur.overlays.default
-            ]; 
-            nixpkgs.config.allowUnfree = true;
-          }
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.alex = import ./home/alex;
-            home-manager.extraSpecialArgs = { hostname = "thinkpad"; };
-          }
-          ./hosts/thinkpad
-        ];
-        specialArgs = { inherit self; };
-      };
-      framework = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          configuration
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.alex = import ./home/alex;
-            home-manager.extraSpecialArgs = { hostname = "framework"; };
-          }
-          ./hosts/framework
-        ];
-        specialArgs = { inherit self inputs; };
-      };
-      desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          configuration
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.alex = import ./home/alex;
-            home-manager.extraSpecialArgs = { hostname = "house-of-wind"; };
-          }
-          ./hosts/desktop
-        ];
-        specialArgs = { inherit self inputs; };
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          nixpkgs.config.allowUnfree = true;
+          nixpkgs.overlays = [
+            nix-vscode-extensions.overlays.default
+            nur.overlays.default
+          ];
+        };
+    in
+    {
+      nixosConfigurations = {
+        thinkpad = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            {
+              nixpkgs.overlays = [
+                nix-vscode-extensions.overlays.default
+                nur.overlays.default
+              ];
+              nixpkgs.config.allowUnfree = true;
+            }
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.alex = import ./home/alex;
+              home-manager.extraSpecialArgs = {
+                hostname = "thinkpad";
+              };
+            }
+            ./hosts/thinkpad
+          ];
+          specialArgs = { inherit self; };
+        };
+        framework = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            disko.nixosModules.disko
+            configuration
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.alex = import ./home/alex;
+              home-manager.extraSpecialArgs = {
+                hostname = "framework";
+              };
+            }
+            ./hosts/framework
+          ];
+          specialArgs = { inherit self inputs; };
+        };
+        desktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            disko.nixosModules.disko
+            sops-nix.nixosModules.sops
+            configuration
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.alex = import ./home/alex;
+              home-manager.extraSpecialArgs = {
+                hostname = "house-of-wind";
+              };
+            }
+            ./hosts/desktop
+          ];
+          specialArgs = { inherit self inputs; };
+        };
       };
     };
-  };
 }
