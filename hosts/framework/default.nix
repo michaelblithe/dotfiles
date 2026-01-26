c336b7cb008e680fd0360e742687779f06ded1f5
Michael Blithe <michaelrblithe@gmail.com>
2026-01-25 21:12:42 -0500

    Thinkpad version fix

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -139,14 +139,5 @@
   ];
 
   home-manager.users.alex.home.stateVersion = "25.11";
-
-  virtualisation.vmVariant = {
-    virtualisation = {
-      memorySize = 8192; # in MB
-      cores = 8;
-      graphics = true;
-    };
-  };
-
   system.stateVersion = "25.11";
 }
--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -6,5 +6,6 @@
     ../common
     
   ];
+  home-manager.users.alex.home.stateVersion = "25.11";
   system.stateVersion = "25.11";
 }
