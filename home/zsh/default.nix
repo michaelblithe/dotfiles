422aa8917b40303e7241bb9792edc7cc26e17b32
Alex Blithe <michaelrblithe@gmail.com>
2026-02-13 22:25:38 -0500

    Useful stuff

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -8,5 +8,10 @@
         theme = "lambda";
         plugins = ["git"];
       };
+      shellAliases = {
+        vpn-up = "sudo systemctl stop wg-quick-wg1 2>/dev/null; sudo systemctl start wg-quick-wg0";
+        vpn-up-2 = "sudo systemctl stop wg-quick-wg0 2>/dev/null; sudo systemctl start wg-quick-wg1";
+        vpn-down = "sudo systemctl stop wg-quick-wg0 2>/dev/null; sudo systemctl stop wg-quick-wg1 2>/dev/null";
+      };
     };
 }
