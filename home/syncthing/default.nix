e0dd4f96d59c475e7c47496ed693073a0717392c
Michael Blithe <michaelrblithe@gmail.com>
2026-03-07 20:37:22 -0500

    Luna and iphone syncthing devices and folders.

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -15,6 +15,12 @@
         "desktop" = {
           id = "3HGNBT7-T35ALSL-TSTEVOL-77IHHNB-44G7GU7-UPADBA3-J2TFXYN-2WWHOQF";
         };
+        "luna" = {
+          id = "DNWN7TQ-74ZA7R7-Z4JYJUU-7SDIGLY-FBY4A4K-YZFBXC7-FXUBPUG-5MR6AAQ";
+        };
+        "iphone" = {
+          id = "X3JAU7A-7VLYXVQ-VG7ZBVV-Q2MGYRR-VIFWJXD-MERQSYC-W36NSKQ-VUKVGAR";
+        };
       };
       folders = {
         "notes" = {
@@ -23,6 +29,8 @@
             "framework"
             "macbook"
             "desktop"
+            "luna"
+            "iphone"
           ];
         };
         "games" = {
@@ -31,6 +39,8 @@
             "framework"
             "macbook"
             "desktop"
+            "luna"
+            "iphone"
           ];
         };
         "c7bzt-7ejmj" = {
@@ -40,6 +50,8 @@
             "framework"
             "macbook"
             "desktop"
+            "luna"
+            "iphone"
           ];
         };
       };
