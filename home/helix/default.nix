6e8f48b4830988b184b36e2747ee9cefb0bc06b2
Michael Blithe <michaelrblithe@gmail.com>
2026-04-13 21:53:34 -0400

    Helix

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -1,7 +1,17 @@
-{pkgs, catppuccin, ...}:
+{ pkgs, catppuccin, ... }:
 
 {
   programs.helix = {
     enable = true;
+    settings = {
+      theme = "catppuccin_frappe";
+      keys.normal = {
+        "^" = "goto_first_nonwhitespace";
+        "$" = "goto_line_end";
+        "g"."g" = "goto_file_start";
+        "G" = "goto_file_end";
+        "a" = "append_mode";
+      };
+    };
   };
 }
