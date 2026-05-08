be53d6cd5dd98d00768edb56e48a58a06d66b883
Michael Blithe <michaelrblithe@gmail.com>
2026-05-08 17:02:51 -0400

    Stuffs

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,4 +1,5 @@
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('jtdls')
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("jtdls")
+vim.lsp.enable("nixdls")
 
 vim.o.autocomplete = true
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,14 @@
 vim.pack.add({
-    {
-        src = 'https://github.com/nvim-mini/mini.nvim',
-        version = 'v0.17.0'
-    },
-    { 
-        src = 'https://github.com/neovim/nvim-lspconfig',
-        version = 'v2.8.0'
-    },
+	{
+		src = "https://github.com/nvim-mini/mini.nvim",
+		version = "v0.17.0",
+	},
+	{
+		src = "https://github.com/neovim/nvim-lspconfig",
+		version = "v2.9.0",
+	},
 })
 
-require('mini.pick').setup()
-require('mini.completion').setup()
+require("mini.pick").setup()
+require("mini.completion").setup()
+
