f9bb2b27b225dda57e63288d0c70246b6167f0b8
Alex Blithe <michaelrblithe@gmail.com>
2026-05-14 19:25:21 -0400

    Gnome setup

--- home/gnome/default.nix
+++ home/gnome/default.nix
@@ -1,8 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  # environment.variables = {
-  #   GDK_SCALE = "1.5";
-  #   GDK_DPI_SCALE = "0.75";
-  # };
+  dconf.settings = {
+    # Enable fractional scaling
+    "org/gnome/mutter" = {
+      experimental-features = [ "scale-monitor-framebuffer" ];
+    };
+
+    # Dark mode, scaling, and battery percentage
+    "org/gnome/desktop/interface" = {
+      text-scaling-factor = 1.3;  # 1.3 fractional scaling (change to 1.25 if needed)
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+    };
+
+    # Power settings - power saver profile
+    "org/gnome/settings-daemon/plugins/power" = {
+      power-button-action = "suspend";
+      power-saver-profile-on-low-battery = true;
+    };
+  };
 }
