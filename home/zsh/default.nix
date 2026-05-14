63c2ac969f51f3a18fb9974963c1203447d28127
Alex Blithe <michaelrblithe@gmail.com>
2026-05-14 19:27:29 -0400

    Update configuration files and add .envrc for flake support

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
     programs.zsh = {
@@ -9,6 +9,7 @@
         plugins = ["git"];
       };
       shellAliases = {
+      } // lib.optionalAttrs (pkgs.stdenv.isLinux) {
         vpn-up = "sudo systemctl stop wg-quick-wg1 2>/dev/null; sudo systemctl start wg-quick-wg0";
         vpn-up-2 = "sudo systemctl stop wg-quick-wg0 2>/dev/null; sudo systemctl start wg-quick-wg1";
         vpn-down = "sudo systemctl stop wg-quick-wg0 2>/dev/null; sudo systemctl stop wg-quick-wg1 2>/dev/null";
