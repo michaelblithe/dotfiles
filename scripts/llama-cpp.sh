77ce9387e9f07265ec8b72e372f721971ca93e0f
Alex Blithe <michaelrblithe@gmail.com>
2026-05-14 19:30:54 -0400

    Add claude-local function to manage Anthropics models and configurations

--- scripts/llama-cpp.sh
+++ scripts/llama-cpp.sh
@@ -16,3 +16,17 @@ unload-model() {
   local model=$1
   http POST $LLAMA_CPP_URL/models/unload model="$model"
 }
+
+claude-local() {
+  local opus_model=$1
+  local sonnet_model={$2:-$opus_model}
+  local haiku_model={$3:-$sonnet_model}
+  local code_subagent_model={$4:-$haiku_model}
+  export ANTHROPIC_BASE_URL=http://house-of-wind:8001
+  export ANTHROPIC_MODEL=$sonnet_model
+  export ANTHROPIC_DEFAULT_OPUS_MODEL=$opus_model
+  export ANTHROPIC_DEFAULT_SONNET_MODEL=$sonnet_model
+  export ANTHROPIC_DEFAULT_HAIKU_MODEL=$haiku_model
+  export CLAUDE_CODE_SUBAGENT_MODEL=$code_subagent_model
+  export ENABLE_TOOL_SEARCH=false
+}
