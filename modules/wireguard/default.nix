3c1a863660a28400789cd6ab47d152f5439f2490
Alex Blithe <michaelrblithe@gmail.com>
2026-05-14 19:27:29 -0400

    Fixing wireguard

--- modules/wireguard/default.nix
+++ modules/wireguard/default.nix
@@ -19,12 +19,12 @@ in
   networking.wg-quick.interfaces.wg0 = commonConfig // {
     peers = [
       {
-        publicKey = "umCaW98SBPbNjApBKCo0ReYhT2AJ0QfV/ZlyWnWmVUk=";
+        publicKey = "eAXSF+LlakN4YOXCSTO/C+FHrwxISh/ekH+BuSjLZgY=";
         allowedIPs = [
           "0.0.0.0/0"
           "::/0"
         ];
-        endpoint = "149.102.226.225:51820";
+        endpoint = "151.243.141.6:51820";
         persistentKeepalive = 25;
       }
     ];
@@ -33,12 +33,12 @@ in
   networking.wg-quick.interfaces.wg1 = commonConfig // {
     peers = [
       {
-        publicKey = "eAXSF+LlakN4YOXCSTO/C+FHrwxISh/ekH+BuSjLZgY=";
+        publicKey = "umCaW98SBPbNjApBKCo0ReYhT2AJ0QfV/ZlyWnWmVUk=";
         allowedIPs = [
           "0.0.0.0/0"
           "::/0"
         ];
-        endpoint = "151.243.141.6:51820";
+        endpoint = "149.102.226.225:51820";
         persistentKeepalive = 25;
       }
     ];
