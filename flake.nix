38a16d894c5be2cc6000d680de8b33fcca22ebb6
Alex Blithe <michaelrblithe@gmail.com>
2026-05-14 19:30:54 -0400

    Fixing the shell for nixfmt in the devShells to ensure it is available for development.

--- flake.nix
+++ flake.nix
@@ -209,7 +209,8 @@
         specialArgs = { inherit inputs self; };
       };
 
-      devShells = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (system:
+      devShells = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
@@ -217,6 +218,7 @@
           default = pkgs.mkShell {
             packages = with pkgs; [
               nil
+              nixfmt
               nixpkgs-fmt
               statix
               deadnix
